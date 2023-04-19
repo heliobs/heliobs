e9f643949cf8ecb08945c6d8c6596828356c4787
Helio Santana <118546235+heliobs@users.noreply.github.com>
2023-04-19 22:12:57 +0100

    Add files via upload

---
+++ SmartHealth/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("java")
+    id("com.google.protobuf") version "0.9.2"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.protobuf:protobuf-java:3.21.7")
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
